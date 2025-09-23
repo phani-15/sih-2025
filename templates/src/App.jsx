83476963b2373812b135a5f67d50fb7e75e974b5
unknown <phanipolavarapu15@gmail.com>
2025-09-24 00:14:06 +0530

    some changes

---
+++ templates/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// import Form from './components/form'
+import InternshipForm from './components/InternshipForm'
+
+export default function App() {
+  return (
+    <div >
+      {/* <Form/> */} 
+      <InternshipForm/>
+    </div>
+  )
+}
